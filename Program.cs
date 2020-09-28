64fa0cbc9041c51a483c2a143380410a984ac5bc
Faiz <faiz.anwar4749@gmail.com>
2020-09-28 22:43:36 +0530

    Added welcome file

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace master
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.ReadKey();
+        }
+    }
+}
