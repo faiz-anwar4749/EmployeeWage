44c9d1dbd89263a9100159cf9baf1740f1ee388e
Faiz <faiz.anwar4749@gmail.com>
2020-09-28 23:43:00 +0530

    [Faiz] Committing MonthlyWage Program (UC5)

--- Program.cs
+++ Program.cs
@@ -9,23 +9,27 @@ namespace master
             int IS_FULL_TIME = 2;
             int IS_PART_TIME = 1;
             int EMP_WAGE_PER_HOUR = 20;
-            int EmpHours, EmpWage;
+            int WorkingDays = 20;
+            int EmpHours, EmpWage = 0;
             Random rand = new Random();
-            int check = rand.Next(0, 3);
-            switch (check)
+            for (int i = 0; i < WorkingDays; i++)
             {
-                case 2:
-                    EmpHours = 8;
-                    break;
-                case 1:
-                    EmpHours = 4;
-                    break;
-                default:
-                    EmpHours = 0;
-                    break;
+                int check = rand.Next(0, 3);
+                switch (check)
+                {
+                    case 2:
+                        EmpHours = 8;
+                        break;
+                    case 1:
+                        EmpHours = 4;
+                        break;
+                    default:
+                        EmpHours = 0;
+                        break;
+                }
+                EmpWage += EmpHours * EMP_WAGE_PER_HOUR;
             }
-            EmpWage = EmpHours * EMP_WAGE_PER_HOUR;
-            Console.WriteLine("Employee Wages: " + EmpWage);
+            Console.WriteLine("Employee Wage: " + EmpWage);
         }
     }
 }
